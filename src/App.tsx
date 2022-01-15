7d48013301ea6e4719455f108e92481087028f84
cuzekn <cuzekn@gmail.com>
2022-01-15 22:28:01 +0900

    chore: Tailwindcssの環境構築

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './dist.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-green-600 text-7">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
